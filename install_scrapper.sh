ed029a8bb7a396d01af7c21fe52d8c158b1cc836
Diogo Watson <diogowatson@gmail.com>
2020-10-23 15:54:08 -0400

    Add files via upload
    add bash script to install al requirements in gcp VM

---
+++ install_scrapper.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+sudo apt update
+sudo apt -y upgrade
+sudo apt install -y python3-pip
+pip3 install scrapy
+pip3 install dateutil
+echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
+sudo apt-get install apt-transport-https ca-certificates gnupg
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key --keyring /usr/share/keyrings/cloud.google.gpg add -
+sudo apt-get update && sudo apt-get install google-cloud-sdk
